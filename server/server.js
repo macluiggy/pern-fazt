5d83111b4f57a4ac19e48910fd5036a09a981568
macluiggy <ferrinluiggy@gmail.com>
2021-10-14 07:21:56 -0500

    jejejej

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ import express from 'express'
 import { MongoClient } from 'mongodb'
 import template from './../template'
 //comment out before building for production
-import devBundle from './devBundle'
+//import devBundle from './devBundle'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express()
 //comment out before building for production
-devBundle.compile(app)
+//devBundle.compile(app)
 
 const CURRENT_WORKING_DIR = process.cwd()
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
