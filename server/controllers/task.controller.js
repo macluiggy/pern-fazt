c0b71e19057fd4e80b3136abc97536725c48f7c3
luiggy macias <ferrinluiggy@gmail.com>
2021-12-15 15:19:13 -0500

    djjd

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -1,9 +1,11 @@
 import pool from "../database";
-
+import dotenv from "dotenv";
+dotenv.config();
 export const test = async (req, res) => {
   // res.send("Hello World");
   try {
     const response = await pool.query("SELECT * FROM users");
+    console.log(process.env.USER);
     res.json(response.rows);
   } catch (error) {
     console.log(error);
--- server/database.js
+++ server/database.js
@@ -1,10 +1,12 @@
 import { Pool } from "pg";
 import dotenv from "dotenv";
+dotenv.config();
+
 export var pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  password: "",
-  database: "tasks_pern",
+  user: "ersacuizzxfjfn",
+  host: "ec2-3-220-240-189.compute-1.amazonaws.com",
+  password: "fb05ad5f0bffa54fc5087ee645c13a3069165f71236be4f14f0761acd18054ae",
+  database: "d2le5gil7i6cmj",
   port: 5432,
 });
 
