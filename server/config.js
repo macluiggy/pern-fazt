4648c70a4dd339b3e7e2a44291ac5420bbe190cf
luiggy macias <ferrinluiggy@gmail.com>
2021-12-16 13:49:48 -0500

    adding configs

---
+++ server/config.js
@@ -0,0 +1,11 @@
+// const isProduction = process.env.NODE_ENV === "production";
+// const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+import dotenv from "dotenv";
+dotenv.config();
+
+export const db = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
--- server/database.js
+++ server/database.js
@@ -43,8 +43,13 @@
 // // });
 // export default pool;
 
-const Pool = require("pg").Pool;
-require("dotenv").config();
+// const Pool = require("pg").Pool;
+// require("dotenv").config();
+import { db } from "./config";
+import { Pool } from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
 // const isProduction = process.env.NODE_ENV === "production";
 // const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 // const pool = new Pool({
@@ -54,20 +59,13 @@ require("dotenv").config();
 //   },
 // });
 
-var pool = new Pool({
-  connectionString:
-    "postgres://ersacuizzxfjfn:fb05ad5f0bffa54fc5087ee645c13a3069165f71236be4f14f0761acd18054ae@ec2-3-220-240-189.compute-1.amazonaws.com:5432/d2le5gil7i6cmj",
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-module.exports = pool;
+// var pool = new Pool({
+//   connectionString:
+//     "postgres://ersacuizzxfjfn:fb05ad5f0bffa54fc5087ee645c13a3069165f71236be4f14f0761acd18054ae@ec2-3-220-240-189.compute-1.amazonaws.com:5432/d2le5gil7i6cmj",
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
 
-// make a pool connection to the database
-const pool2 = new Pool({
-  user: "postgres",
-  host: "localhost",
-  password: "",
-  database: "tasks_pern",
-  port: 5432,
-});
+const pool = new Pool(db);
+export default pool;
