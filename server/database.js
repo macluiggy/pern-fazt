87470e1491354ba112307adf3f3d8478c1fb0eb0
luiggy macias <ferrinluiggy@gmail.com>
2021-12-16 07:17:13 -0500

    jdjdj

--- server/database.js
+++ server/database.js
@@ -45,10 +45,18 @@
 
 const Pool = require("pg").Pool;
 require("dotenv").config();
-const isProduction = process.env.NODE_ENV === "production";
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+// const isProduction = process.env.NODE_ENV === "production";
+// const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+// const pool = new Pool({
+//   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+var pool = new Pool({
+  connectionString:
+    "postgres://ersacuizzxfjfn:fb05ad5f0bffa54fc5087ee645c13a3069165f71236be4f14f0761acd18054ae@ec2-3-220-240-189.compute-1.amazonaws.com:5432/d2le5gil7i6cmj",
   ssl: {
     rejectUnauthorized: false,
   },
