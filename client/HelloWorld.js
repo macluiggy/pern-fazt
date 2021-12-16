a725f19f5a948861add0bfbfe91103b045581110
luiggy macias <ferrinluiggy@gmail.com>
2021-12-16 15:49:42 -0500

    djdj

--- client/HelloWorld.js
+++ client/HelloWorld.js
@@ -4,20 +4,21 @@ import { BrowserRouter, Link, Route, Router, Routes } from "react-router-dom";
 import { Container } from "@mui/material";
 // components
 import TaskForm from "./components/TaskForm";
-import Navbar from "./components/Navbar";
+import Menu from "./components/Navbar";
 import TaskList from "./components/TaskList";
 
 const HelloWorld = () => {
   return (
     <BrowserRouter>
-      <h1>Hello World, como estan todos dddheshedd</h1>
-      {/* <Navbar /> */}
-      <Link to="/tasks/new">new task</Link>
-      <Link to="/">Home</Link>
-      <Routes>
-        <Route path="/" element={<TaskList />} />
-        <Route path="/tasks/new" element={<TaskForm />} />
-      </Routes>
+      <Menu />
+      <Container>
+        <Link to="/tasks/new">new task</Link>
+        <Link to="/">Home</Link>
+        <Routes>
+          <Route path="/" element={<TaskList />} />
+          <Route path="/tasks/new" element={<TaskForm />} />
+        </Routes>
+      </Container>
     </BrowserRouter>
   );
 };
--- client/components/Navbar/index.js
+++ client/components/Navbar/index.js
@@ -1,5 +1,26 @@
 import React from "react";
+import {
+  Button,
+  Box,
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <Box>
+      <ThemeProvider>
+        <AppBar position="static" color="tranparent">
+          <Container>
+            <Toolbar>
+              <Typography>PERN STACK</Typography>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </ThemeProvider>
+    </Box>
+  );
 }
